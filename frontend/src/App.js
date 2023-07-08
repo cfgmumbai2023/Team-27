d332c9eb934f4151f0e7a5d7f1199096841996c2
Aayushk1862 <aayushk1862@gmail.com>
2023-07-09 02:37:13 +0530

    login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import UserRegisteration from './pages/UserRegisteration';
+import CreatorRegisteration from './pages/CreatorRegisteration';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <UserRegisteration />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/registerStudent' element={<UserRegisteration/>}/>
+          <Route path='/registerCreator' element={<CreatorRegisteration/>}/>
+
+        </Routes>
+
+        
+        
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/UserRegisteration.js
+++ frontend/src/pages/UserRegisteration.js
@@ -1,6 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
 
 const UserRegisteration = () => {
+
+
+    const navigate = useNavigate()
+
+    const [ user, setUser] = useState({
+        name: "",
+        email:"",
+        password:"",
+        classes:"",
+        age:"",
+        school:"",
+        contactnumber:"",
+        address:"",
+        uidse:"",
+        language1:"",
+        language2: "",
+        type:"Student"
+    })
+
+    const handleChange = e => {
+        const { name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+      const register = (e) => {
+        e.preventDefault()
+        console.log(user)
+        const { name, email, password } = user
+        if( name && email && password ){
+            axios.post("http://localhost:5000/adduser", {
+              type: user.type,
+              name: user.name,
+              email: user.email,
+              password: user.password,
+              classes: user.classes,
+              age: user.age,
+              school: user.school,
+              contactnumber: user.contactnumber,
+              address: user.address,
+              uidse: user.uidse,
+              language1: user.language1,
+              language2: user.language2
+            }).then( res => {
+                alert("Successful Registration")
+                console.log(res.data.Users)
+                console.log("Yippee")
+              }).catch((err) => console.log(err))
+        } else {
+            alert("invlid input")
+        }
+        
+    }  
   return (
     
       <section className="signup">
@@ -9,7 +67,7 @@ const UserRegisteration = () => {
     <div className="signup-content">
       <div className="signup-form">
         <h2 className="form-title">Sign up</h2>
-        <form className="user-register" id="user-register">
+        <form className="user-register" id="user-register" onChange={handleChange}>
 
           <div className="mb-3">
             <input type="text" name="name" id="name" className="form-control" autoComplete="off" placeholder="Name" />
@@ -44,72 +102,72 @@ const UserRegisteration = () => {
           </div>
 
           <div className="mb-3">
-            <input type="text" name="uidse" id="uidse" className="form-control" autoComplete="off" placeholder="UIDSE" />
+            <input type="text" name="UDISE" id="UDISE" className="form-control" autoComplete="off" placeholder="UDISE" />
           </div>
 
           <div className="mb-3">
   
-  <select name="language1" id="language1" className="form-control">
-    <option value="">Preferred language-1</option>
-    <option value="Hindi">Hindi</option>
-    <option value="Bengali">Bengali</option>
-    <option value="Telugu">Telugu</option>
-    <option value="Marathi">Marathi</option>
-    <option value="Tamil">Tamil</option>
-    <option value="Urdu">Urdu</option>
-    <option value="Gujarati">Gujarati</option>
-    <option value="Kannada">Kannada</option>
-    <option value="Odia">Odia</option>
-    <option value="Punjabi">Punjabi</option>
-    <option value="Malayalam">Malayalam</option>
-    <option value="Assamese">Assamese</option>
-    <option value="Maithili">Maithili</option>
-    <option value="Santali">Santali</option>
-    <option value="Kashmiri">Kashmiri</option>
-    <option value="Nepali">Nepali</option>
-    <option value="Sindhi">Sindhi</option>
-    <option value="Konkani">Konkani</option>
-    <option value="Manipuri">Manipuri</option>
-    <option value="Dogri">Dogri</option>
-    <option value="Bodo">Bodo</option>
-    <option value="Sanskrit">Sanskrit</option>
-  </select>
-</div>
+            <select name="language1" id="language1" className="form-control">
+              <option value="">Preferred language-1</option>
+              <option value="Hindi">Hindi</option>
+              <option value="Bengali">Bengali</option>
+              <option value="Telugu">Telugu</option>
+              <option value="Marathi">Marathi</option>
+              <option value="Tamil">Tamil</option>
+              <option value="Urdu">Urdu</option>
+              <option value="Gujarati">Gujarati</option>
+              <option value="Kannada">Kannada</option>
+              <option value="Odia">Odia</option>
+              <option value="Punjabi">Punjabi</option>
+              <option value="Malayalam">Malayalam</option>
+              <option value="Assamese">Assamese</option>
+              <option value="Maithili">Maithili</option>
+              <option value="Santali">Santali</option>
+              <option value="Kashmiri">Kashmiri</option>
+              <option value="Nepali">Nepali</option>
+              <option value="Sindhi">Sindhi</option>
+              <option value="Konkani">Konkani</option>
+              <option value="Manipuri">Manipuri</option>
+              <option value="Dogri">Dogri</option>
+              <option value="Bodo">Bodo</option>
+              <option value="Sanskrit">English</option>
+            </select>
+          </div>
 
 
            <div className="mb-3">
   
-  <select name="language2" id="language2" className="form-control">
-    <option value="">Preferred language-2</option>
-    <option value="Hindi">Hindi</option>
-    <option value="Bengali">Bengali</option>
-    <option value="Telugu">Telugu</option>
-    <option value="Marathi">Marathi</option>
-    <option value="Tamil">Tamil</option>
-    <option value="Urdu">Urdu</option>
-    <option value="Gujarati">Gujarati</option>
-    <option value="Kannada">Kannada</option>
-    <option value="Odia">Odia</option>
-    <option value="Punjabi">Punjabi</option>
-    <option value="Malayalam">Malayalam</option>
-    <option value="Assamese">Assamese</option>
-    <option value="Maithili">Maithili</option>
-    <option value="Santali">Santali</option>
-    <option value="Kashmiri">Kashmiri</option>
-    <option value="Nepali">Nepali</option>
-    <option value="Sindhi">Sindhi</option>
-    <option value="Konkani">Konkani</option>
-    <option value="Manipuri">Manipuri</option>
-    <option value="Dogri">Dogri</option>
-    <option value="Bodo">Bodo</option>
-    <option value="Sanskrit">Sanskrit</option>
-  </select>
-</div>
+            <select name="language2" id="language2" className="form-control">
+              <option value="">Preferred language-2</option>
+              <option value="Hindi">Hindi</option>
+              <option value="Bengali">Bengali</option>
+              <option value="Telugu">Telugu</option>
+              <option value="Marathi">Marathi</option>
+              <option value="Tamil">Tamil</option>
+              <option value="Urdu">Urdu</option>
+              <option value="Gujarati">Gujarati</option>
+              <option value="Kannada">Kannada</option>
+              <option value="Odia">Odia</option>
+              <option value="Punjabi">Punjabi</option>
+              <option value="Malayalam">Malayalam</option>
+              <option value="Assamese">Assamese</option>
+              <option value="Maithili">Maithili</option>
+              <option value="Santali">Santali</option>
+              <option value="Kashmiri">Kashmiri</option>
+              <option value="Nepali">Nepali</option>
+              <option value="Sindhi">Sindhi</option>
+              <option value="Konkani">Konkani</option>
+              <option value="Manipuri">Manipuri</option>
+              <option value="Dogri">Dogri</option>
+              <option value="Bodo">Bodo</option>
+              <option value="Sanskrit">English</option>
+            </select>
+          </div>
 
 
 
           <div className="text-center">
-            <input type="submit" name="signup" id="signup" className="btn btn-primary" value="Register as USER" />
+            <button type="submit" name="Register" id="Register" className="btn btn-primary" value="Register" onClick={register}>Register</button>
           </div>
 
         </form>
@@ -125,4 +183,5 @@ const UserRegisteration = () => {
   )
 }
 
-export default UserRegisteration 
+
+export default UserRegisteration;
